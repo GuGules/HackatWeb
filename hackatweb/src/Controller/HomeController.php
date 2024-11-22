5756f444512ff1295f9ad00fc9a05e2aee6be643
Jules PILLOT <jules.pillot85@gmail.com>
2024-11-22 20:09:22 +0100

    Update & Config

--- hackatweb/src/Controller/HomeController.php
+++ hackatweb/src/Controller/HomeController.php
@@ -11,27 +11,33 @@ use DateTime;
 
 class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
+    #[Route('', name: 'app_home')]
     public function index(EntityManagerInterface $em): Response
+    {    
+        return $this->render('home.html.twig');
+    }
+
+    #[Route('/hackathons', name: 'app_hackathons')]
+    public function showHackathons(EntityManagerInterface $em): Response
     {
         $repository = $em->getRepository(Hackathon::class);
         $lesHackathonsTrie = $repository->findBy([],['date_debut' => 'ASC']);
         $lesHackathons = $repository->findAll();
     
-        return $this->render('home/index.html.twig', [
+        return $this->render('hackathon/index.html.twig', [
             'lesHackathons' => $lesHackathons,
             'lesHackathonsAfficher' => $lesHackathons,
             'lesHackathonsTrie' => $lesHackathonsTrie
         ]);
     }
     
-    #[Route('/home/{id}', name: 'app_unHackathon')]
+    #[Route('/hackathons/{id}', name: 'app_unHackathon')]
     public function showOneSerie(EntityManagerInterface $em, int $id): Response
     {
         $repository = $em->getRepository(Hackathon::class);
         $leHackathon = $repository->find($id);
     
-        return $this->render('home/details.html.twig', [
+        return $this->render('hackathon/details.html.twig', [
             'leHackathon' => $leHackathon,
         ]);
     }
